8236bc51f12783b3020b34b1f04020cb5599e568
hallier <ahallier@isara.fr>
2021-02-05 20:24:18 +0100

    Feat : modify functionality modify method getPerson to treat the person nonexistence

--- src/main/java/com/openclassrooms/safetynet/service/PersonService.java
+++ src/main/java/com/openclassrooms/safetynet/service/PersonService.java
@@ -4,6 +4,8 @@ import com.openclassrooms.safetynet.model.Person;
 import com.openclassrooms.safetynet.repository.PersonRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
 import java.util.Optional;
 import lombok.Data;
 
@@ -14,8 +16,8 @@ public class PersonService {
   @Autowired
   private PersonRepository personRepository;
 
-  public Optional<Person> getPerson(final Long id) {
-    return personRepository.findById(id);
+  public Person getPerson(final Long id) {
+    return personRepository.findById(id).orElseThrow(() -> new NoSuchElementException("la personne " + id + " n'existe pas"));
   }
 
   public Iterable<Person> getPersons() {
