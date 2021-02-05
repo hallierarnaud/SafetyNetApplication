ccfb2c293315074f491a65020941a9689d91f0e0
hallier <ahallier@isara.fr>
2021-02-05 20:18:03 +0100

    Feat : add functionality add a method @GetMapping("/persons/{id}") in the controller to obtain a person contained in the database by his id

--- src/main/java/com/openclassrooms/safetynet/controller/PersonController.java
+++ src/main/java/com/openclassrooms/safetynet/controller/PersonController.java
@@ -3,8 +3,11 @@ package com.openclassrooms.safetynet.controller;
 import com.openclassrooms.safetynet.model.Person;
 import com.openclassrooms.safetynet.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.NoSuchElementException;
 
 @RestController
 public class PersonController {
@@ -15,6 +18,15 @@ public class PersonController {
   @GetMapping("/persons")
   public Iterable<Person> getPersons() {
     return personService.getPersons();
+  }
+
+  @GetMapping("/persons/{id}")
+  public ResponseEntity<Person> getPersonById(@PathVariable("id") long id) {
+    try {
+      return ResponseEntity.ok(personService.getPerson(id));
+    } catch (NoSuchElementException e) {
+      return ResponseEntity.notFound().build();
+    }
   }
 
 }
