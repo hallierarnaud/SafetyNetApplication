213e9841c98d427dd3a329c63a48453662098bb2
hallier <ahallier@isara.fr>
2021-02-05 10:27:49 +0100

    Feat : database/application communication add of the CrudRepository interface

---
+++ src/main/java/com/openclassrooms/safetynet/repository/PersonRepository.java
@@ -0,0 +1,10 @@
+package com.openclassrooms.safetynet.repository;
+
+import com.openclassrooms.safetynet.model.Person;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface PersonRepository extends CrudRepository<Person, Long> {
+
+}
