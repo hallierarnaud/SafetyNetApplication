19f9a3656045e484c473f91017eeaae0973a6780
hallier <ahallier@isara.fr>
2021-02-07 18:43:19 +0100

    Feat : add functionality add a method @PostMapping in a controller to add a person in the database

--- src/main/java/com/openclassrooms/safetynet/controller/PersonController.java
+++ src/main/java/com/openclassrooms/safetynet/controller/PersonController.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import java.util.NoSuchElementException;
 
@@ -27,6 +29,20 @@ public class PersonController {
     } catch (NoSuchElementException e) {
       return ResponseEntity.notFound().build();
     }
+  }
+
+  @PostMapping("/add")
+  public String addPerson(@RequestParam String firstName, @RequestParam String lastName, @RequestParam String address, @RequestParam String city, @RequestParam String zip, @RequestParam String phone, @RequestParam String email) {
+    Person person = new Person();
+    person.setFirstName(firstName);
+    person.setLastName(lastName);
+    person.setAddress(address);
+    person.setCity(city);
+    person.setZip(zip);
+    person.setPhone(phone);
+    person.setEmail(email);
+    personService.savePerson(person);
+    return "Added new person to repo !";
   }
 
 }
