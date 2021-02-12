3fcc0c565b46f4e136447aea5fc8c7fc616247ad
hallier <ahallier@isara.fr>
2021-02-12 11:55:30 +0100

    Feat : add functionalities add methods getFireStation, getFireStations, deleteFireStation and saveFireStation in FireStationService.java add methods getMedicalRecord, getMedicalRecords, deleteMedicalRecord and saveMedicalRecord in MedicalRecordService.java

--- src/main/java/com/openclassrooms/safetynet/service/FireStationService.java
+++ src/main/java/com/openclassrooms/safetynet/service/FireStationService.java
@@ -1,4 +1,39 @@
 package com.openclassrooms.safetynet.service;
 
+import com.openclassrooms.safetynet.model.FireStation;
+import com.openclassrooms.safetynet.repository.FireStationRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+import lombok.Data;
+
+@Data
+@Service
 public class FireStationService {
+
+  @Autowired
+  private FireStationRepository fireStationRepository;
+
+  public FireStation getFireStation(final Long id) {
+    return fireStationRepository.findById(id).orElseThrow(() -> new NoSuchElementException("la caserne " + id + " n'existe pas"));
+  }
+
+  public Iterable<FireStation> getFireStations() {
+    return fireStationRepository.findAll();
+  }
+
+  public void deleteFireStation(final Long id) {
+    if (fireStationRepository.existsById(id)) {
+      fireStationRepository.deleteById(id);
+    }
+  }
+
+  public FireStation saveFireStation(FireStation fireStation) {
+    FireStation savedFireStation = fireStationRepository.save(fireStation);
+    return savedFireStation;
+  }
+
 }
--- src/main/java/com/openclassrooms/safetynet/service/MedicalRecordService.java
+++ src/main/java/com/openclassrooms/safetynet/service/MedicalRecordService.java
@@ -1,4 +1,39 @@
 package com.openclassrooms.safetynet.service;
 
+import com.openclassrooms.safetynet.model.MedicalRecord;
+import com.openclassrooms.safetynet.repository.MedicalRecordRepository;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.NoSuchElementException;
+
+import lombok.Data;
+
+@Data
+@Service
 public class MedicalRecordService {
+
+  @Autowired
+  private MedicalRecordRepository medicalRecordRepository;
+
+  public MedicalRecord getMedicalRecord(final Long id) {
+    return medicalRecordRepository.findById(id).orElseThrow(() -> new NoSuchElementException("les données santé " + id + " n'existent pas"));
+  }
+
+  public Iterable<MedicalRecord> getMedicalRecords() {
+    return medicalRecordRepository.findAll();
+  }
+
+  public void deleteMedicalRecord(final Long id) {
+    if (medicalRecordRepository.existsById(id)) {
+      medicalRecordRepository.deleteById(id);
+    }
+  }
+
+  public MedicalRecord saveMedicalRecord(MedicalRecord medicalRecord) {
+    MedicalRecord savedMedicalRecord = medicalRecordRepository.save(medicalRecord);
+    return savedMedicalRecord;
+  }
+
 }
