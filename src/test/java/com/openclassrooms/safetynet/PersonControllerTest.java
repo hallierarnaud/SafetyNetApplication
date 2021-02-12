d9285e465bb4a452c6a85f3663c365f8924f220f
hallier <ahallier@isara.fr>
2021-02-12 10:19:01 +0100

    Test : unitary test add test of Post method for PersonController

--- src/test/java/com/openclassrooms/safetynet/PersonControllerTest.java
+++ src/test/java/com/openclassrooms/safetynet/PersonControllerTest.java
@@ -3,19 +3,17 @@ package com.openclassrooms.safetynet;
 import com.openclassrooms.safetynet.controller.PersonController;
 import com.openclassrooms.safetynet.model.Person;
 import com.openclassrooms.safetynet.service.PersonService;
-
-import org.hibernate.collection.internal.PersistentBag;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
-
-import java.util.Iterator;
 import java.util.NoSuchElementException;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @WebMvcTest(controllers = PersonController.class)
@@ -43,6 +41,20 @@ public class PersonControllerTest {
   public void getPersonById_shouldReturnNotFound() throws Exception {
     when(personService.getPerson(any())).thenThrow(NoSuchElementException.class);
     mockMvc.perform(get("/persons/1")).andExpect(status().isNotFound());
+  }
+
+  @Test
+  public void postPerson_shouldReturnOk() throws Exception {
+    mockMvc.perform(post("/persons")
+            .param("id", "1")
+            .param("firstName", "Bart")
+            .param("lastName", "Simpson")
+            .param("address", "unknown")
+            .param("city", "SpringVille")
+            .param("zip", "11111")
+            .param("phone", "555-555")
+            .param("email", "bsimpson@email.com"))
+            .andExpect(status().isOk());
   }
 
 }
